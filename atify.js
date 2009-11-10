83e533687d72fd1e80ac269f2c46348eb5c996bc
Sam Aarons <samaarons@gmail.com>
2009-11-09 21:04:20 -0800

    included @ symbol as part of hyperlink.  Mimics what happens with hashtags.

--- atify.js
+++ atify.js
@@ -17,9 +17,9 @@ jQuery.fn.atify = function(options) {
     var html = jQuery(this).html();
     if (html) {
         if (settings.extras) {
-            html = html.replace(/@([A-Za-z0-9_]+)/gi, "@<a href=\"http://twitter.com/$1\">$1</a>(<a class=\"mention\" href=\"http://twitter.com/?status=%40$1\">m</a>,<a class=\"send_dm\" href=\"http://twitter.com/direct_messages/create/$1\">d</a>)");
+            html = html.replace(/@([A-Za-z0-9_]+)/gi, "<a href=\"http://twitter.com/$1\">@$1</a>(<a class=\"mention\" href=\"http://twitter.com/?status=%40$1\">m</a>,<a class=\"send_dm\" href=\"http://twitter.com/direct_messages/create/$1\">d</a>)");
         } else {
-            html = html.replace(/@([A-Za-z0-9_]+)/gi, "@<a href=\"http://twitter.com/$1\">$1</a>");
+            html = html.replace(/@([A-Za-z0-9_]+)/gi, "<a href=\"http://twitter.com/$1\">@$1</a>");
         }
         if (settings.hashtag) {
             html = html.replace(/#([A-Za-z0-9_-]+)/gi, "<a href=\"http://twitter.com/search?q=%22$1%22\">#$1</a>");
