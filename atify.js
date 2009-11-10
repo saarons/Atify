a529393220b3fdec871058aac8fe7fdd248d6f16
Sam Aarons <samaarons@gmail.com>
2009-11-09 20:50:17 -0800

    some cleanup

--- atify.js
+++ atify.js
@@ -8,24 +8,22 @@
 *
 */
 
-jQuery.fn.atify = function(customOptions) {
-    var options = jQuery.extend({},
-    jQuery.fn.atify.defaultOptions, customOptions);
+jQuery.fn.atify = function(options) {
+    settings = jQuery.extend({
+        hashtag: false,
+        extras: false
+    },
+    options);
     var html = jQuery(this).html();
     if (html) {
-        if (options.extras) {
+        if (settings.extras) {
             html = html.replace(/@([A-Za-z0-9_]+)/gi, "@<a href=\"http://twitter.com/$1\">$1</a>(<a class=\"mention\" href=\"http://twitter.com/?status=%40$1\">m</a>,<a class=\"send_dm\" href=\"http://twitter.com/direct_messages/create/$1\">d</a>)");
         } else {
             html = html.replace(/@([A-Za-z0-9_]+)/gi, "@<a href=\"http://twitter.com/$1\">$1</a>");
         }
-        if (options.hashtag) {
+        if (settings.hashtag) {
             html = html.replace(/#([A-Za-z0-9_-]+)/gi, "<a href=\"http://twitter.com/search?q=%22$1%22\">#$1</a>");
         }
         jQuery(this).html(html);
     }
-}
-
-jQuery.fn.atify.defaultOptions = {
-    'hashtag': false,
-    'extras': false
-}
+};
